7c77043b24d9397b2a608be337be237c6905a6a8
sumit Nayak <sumitnayak2006@gmail.com>
2026-04-17 19:14:10 +0530

    api update

--- api/contact.js
+++ api/contact.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
 
   try {
     const transporter = nodemailer.createTransport({
-      host: "gmail",
+      service: "gmail",
       port: 465,
       secure: true,
       auth: {
